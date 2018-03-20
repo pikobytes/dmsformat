59b4c14225f3c0653d4f73d232badcfce6625a92
Jacob Mendt <jacobmendt@googlemail.com>
2018-03-20 15:05:35 +0100

    Update module export

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 import { fromDMS, isDMS, toDMS } from './src/dmsformat';
 
+export {fromDMS as fromDMS} from './src/dmsformat';
+export {isDMS as isDMS} from './src/dmsformat';
+export {toDMS as toDMS} from './src/dmsformat';
+
 // module export
 module.exports = {
   fromDMS: fromDMS,
