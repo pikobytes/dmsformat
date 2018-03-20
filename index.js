e3441167bb6bc15ce02dadda4a9b2ad83901ad3d
Jacob Mendt <jacobmendt@googlemail.com>
2018-03-20 15:07:35 +0100

    Update module export

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 'use strict';
-
-import { fromDMS, isDMS, toDMS } from './src/dmsformat';
-
 export {fromDMS as fromDMS} from './src/dmsformat';
 export {isDMS as isDMS} from './src/dmsformat';
 export {toDMS as toDMS} from './src/dmsformat';
-
-// module export
-module.exports = {
-  fromDMS: fromDMS,
-  isDMS: isDMS,
-  toDMS: toDMS,
-};
